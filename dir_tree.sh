cefd2205a47a2a4e2febdd3f57b8a6eb7647bbb6
tradewcs <tradewcs@gmail.com>
2022-07-14 11:26:31 +0300

    edit dir_tree.sh

--- dir_tree.sh
+++ dir_tree.sh
@@ -19,12 +19,3 @@ read -p "Press Enter to copy files to the root directory..."
 
 cp "Paranchych/PPP/GR2/Myhailo.txt" "Paranchych/"
 cp "Paranchych/PPP/GR2/Paranchych.txt" "Paranchych/"
-
-var_res="$(grep -E "^[a-zA-Z0-9_][0-9a-zA-Z\._-]+[a-zA-Z0-9]@[a-zA-Z0-9][a-zA-Z\._-]+\.[a-zA-Z0-9]{2,3}$" Paranchych/Paranchych.txt)"
-
-if [ "$var_res" == "" ]
-then
-    echo "The email is invalid."
-else
-    echo "$var_res"
-fi
